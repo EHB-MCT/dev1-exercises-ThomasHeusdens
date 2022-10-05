1ecdccee7db76e510b86fb9ebf4472ab2828f23a
Thomas Heusdens <thomas.heusdens@student.ehb.be>
2022-10-05 15:54:08 +0200

    exercice 1-1 write your name

--- exercises/1-functions/1-1-name.js
+++ exercises/1-functions/1-1-name.js
@@ -1,1 +1,70 @@
 "use strict";
+
+drawLine();
+
+function drawLine() {
+
+   let canvas = document.querySelector("canvas");
+   canvas.width = window.innerWidth;
+   canvas.height = window.innerHeight;
+   let context = canvas.getContext("2d");
+
+   context.lineWidth = 5;
+   context.strokeStyle = 'red';
+   context.beginPath();
+   context.moveTo(25,50); 
+   context.lineTo(225,50);
+   context.lineTo(125,50);
+   context.lineTo(125,250);
+   context.stroke(); 
+
+   context.lineWidth = 10;
+   context.strokeStyle = 'blue';
+   context.beginPath();
+   context.moveTo(275,50);
+   context.lineTo(275,250);
+   context.lineTo(275,150);
+   context.lineTo(375,150);
+   context.lineTo(375,50);
+   context.lineTo(375,250);
+   context.stroke();
+
+   context.fillStyle = 'yellow';
+   context.beginPath();
+   context.rect(425,50,100,200)
+   context.fill();
+
+   context.lineWidth = 5;
+   context.strokeStyle = 'pink';
+   context.beginPath();
+   context.moveTo(575,250);
+   context.lineTo(575,50);
+   context.lineTo(650,150);
+   context.lineTo(725,50);
+   context.lineTo(725,250);
+   context.stroke();
+
+   context.lineWidth = 10;
+   context.strokeStyle = 'green';
+   context.beginPath();
+   context.moveTo(775,250);
+   context.lineTo(850,50);
+   context.lineTo(925,250);
+   context.moveTo(925,150);
+   context.lineTo(775,150);
+   context.stroke();
+
+   context.lineWidth = 10;
+   context.strokeStyle = 'purple';
+   context.beginPath();
+   context.moveTo(1125,50);
+   context.lineTo(975,50);
+   context.lineTo(975,150);
+   context.lineTo(1125,150);
+   context.lineTo(1125,260);
+   context.lineTo(15,260);
+   context.stroke();
+
+}
+   
+
